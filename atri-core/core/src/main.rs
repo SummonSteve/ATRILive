df6eab1ba54cce4cc6e128638f4031cec0ac1852
SummonSteve <wmj1301024658@foxmail.com>
2022-03-11 20:51:44 +0800

    Restart the project

---
+++ atri-core/core/src/main.rs
@@ -0,0 +1,33 @@
+use color_eyre::eyre::Result;
+use poem::{
+    web::Html, Route, get, handler, EndpointExt, middleware::Tracing, Server, listener::TcpListener,
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt::init();
+
+    let http_handle = tokio::spawn(async {
+        let app = Route::new().at("/obs", get(obs)).with(Tracing);
+        Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await.expect("HTTP error")
+    });
+
+    let tcp_handle = tokio::spawn(async {
+        //todo!();
+    });
+
+
+    let (http_result, tcp_result) = tokio::join!(http_handle, tcp_handle);
+
+    http_result?;
+    tcp_result?;
+    Ok(())
+}
+
+#[handler]
+fn obs() -> Html<String> {
+    Html("アトリは、高性能ですから!".to_string())
+}
